462ff0d39529c5e60552b96198c0f78168fab57b
erikkaugusto <erik.ahlert@universo.univates.br>
2024-10-14 07:59:10 -0300

    primeiros componentes

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Banner from './componentes/Banner'; // dessa forma vai pegar o index.js dentro da pasta CampoTexto
+import CampoTexto from './componentes/CampoTexto'; 
+
+function App() {
+  return (
+    <div className="App">
+      <Banner/>
+      <CampoTexto/>
+    </div>
+  );
+}
+
+export default App;
